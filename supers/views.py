bad4973329314e55a05441df3a988e24a2bb7966
rossebell <Rosy24dr@outlook.com>
2022-04-05 12:52:36 -0400

    Renamed the super_type property trying to figure out the custom object response. The GET request is up and running

--- supers/views.py
+++ supers/views.py
@@ -1,61 +1,31 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from super_types.models import SuperType
 from .models import Super
 from .serializer import SuperSerializer
 
 
-@api_view(['GET'])
+
+@api_view(['GET', 'POST'])
 def super_list(request):
 
     if request.method == 'GET':
-        supers = Super.objects.all()
-        serializer = SuperSerializer(supers, many=True)
-        return Response (serializer.data, status = status.HTTP_200_OK)
+        villians_heroes = SuperType.objects.all()
+        custom_response_dictionary = {}
+        for super_type in villians_heroes:
+            heroes_villians = Super.objects.filter(super_type_id = super_type.id)
+            supers_serializer = SuperSerializer(heroes_villians, many=True)
+            custom_response_dictionary[super_type.super_type] = supers_serializer.data
+        return Response (custom_response_dictionary)
+
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data = request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        else: 
+            return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
 
 
-# @api_view(['GET'])
-# def super_typerequest):
-#     supers = Super.objects.all()
-#     serializer = SuperSerializer(supers, many=True)
-#     return Response (serializer.data)
-
-# from django.shortcuts import get_object_or_404
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# from rest_framework import status
-# from .serializers import CarSerializer
-# from .models import Car
-# from cars import serializers
-
-# @api_view(['GET', 'POST'])
-# def cars_list(request):
-
-#     if request.method == 'GET':
-#         cars = Car.objects.all()
-#         serializer = CarSerializer(cars, many=True)
-#         return Response(serializer.data)
-
-
-#     elif request.method == 'POST':
-#         serializer = CarSerializer(data=request.data)
-#         serializer.is_valid(raise_exception= True)
-#         serializer.save()
-#         return Response(serializer.data, status = status.HTTP_201_CREATED)
-   
-
-# @api_view(['GET','PUT', 'DELETE'])
-# def car_detail(request, pk):
-#      car = get_object_or_404(Car, pk=pk)
-#      if request.method == 'GET': 
-#         serializers = CarSerializer(car);
-#         return Response(serializers.data)
-#      elif request.method == 'PUT':
-#         serializers = CarSerializer(car, data = request.data)
-#         serializers.is_valid(raise_exception=True)
-#         serializers.save()
-#         return Response(serializers.data)
-#      elif request.method == "DELETE":
-#          car.delete()
-#          return Response(status = status.HTTP_204_NO_CONTENT)
 
